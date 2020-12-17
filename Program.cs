be11e8ab0f5206a5684b113861b673fe473e2d93
Edgaras <baroniunas@gmail.com>
2020-12-17 15:49:24 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Shop_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
